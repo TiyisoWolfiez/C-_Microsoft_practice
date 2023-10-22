69c39c00663c273eb00387098f35f8b19de8a1e5
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-22 22:55:23 +0200

    Create methods.cs

---
+++ Codes_in_C#/methods.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace HelloWorld
+{
+    
+  class Program
+  {
+  
+  	static void SayHello()
+             {
+                 Console.WriteLine("Hello, Tiyiso!");
+             }
+             
+             
+    static void Main(string[] args)
+    {
+    	
+		
+  			SayHello();
+        
+    }
+  }
+}
