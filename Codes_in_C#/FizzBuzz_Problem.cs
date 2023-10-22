dcd0a6cb99b78825a2a14442cf20b0e8e9affb54
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-22 22:44:15 +0200

    Update FizzBuzz_Problem.cs

--- Codes_in_C#/FizzBuzz_Problem.cs
+++ Codes_in_C#/FizzBuzz_Problem.cs
@@ -1,17 +1,27 @@
-int k = 100;
-for(int i = 1; i <= k;i++){
+using System;
 
-    if(i % 3 == 0){
-        Console.WriteLine(i + " - Fizz");
-    }
-    else if(i % 5 == 0){
-        Console.WriteLine(i + " - Buzz");
-    }
-    else if(i % 3 == 0 && i % 5 == 0){
-        Console.WriteLine(i + " - FizzBuzz");
-    }
-    else{
-        Console.WriteLine(i);
-    }
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    	int k = 100;
+		for(int i = 1; i <= k;i++){
 
+    		if(i % 3 == 0){
+        		Console.WriteLine(i + " - Fizz");
+    		}
+    		else if(i % 5 == 0){
+        		Console.WriteLine(i + " - Buzz");
+    		}
+    		else if(i % 3 == 0 && i % 5 == 0){
+        		Console.WriteLine(i + " - FizzBuzz");
+    		}
+    		else{
+        		Console.WriteLine(i);
+    		}
+		}
+    }
+  }
 }
