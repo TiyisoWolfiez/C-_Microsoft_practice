dfc4193fca5ff007f15ac74a4ccccd5cd5571db7
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-22 22:41:55 +0200

    Update how_to_generate_random_numbers.cs

--- Codes_in_C#/how_to_generate_random_numbers.cs
+++ Codes_in_C#/how_to_generate_random_numbers.cs
@@ -1,14 +1,28 @@
-Random random = new Random();
-int current = random.Next(1,11); 
-
-do
+namespace HelloWorld
 {
-    current = random.Next(1, 11);
-    Console.WriteLine(current);
-} while (current != 7); 
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        
+       Random random = new Random();
+        int current = random.Next(1,11);
 
-while(current >= 3){
-    Console.WriteLine(current);
-    current = random.Next(1, 11);
+        do
+        {
+            current = random.Next(1, 11);
+            Console.WriteLine(current);
+        } while (current != 7); 
+        
+        while(current >= 3){
+            Console.WriteLine(current);
+            current = random.Next(1, 11);
+        }
+        Console.WriteLine($"Last number: {current}"); 
+    }
+  }
 }
-Console.WriteLine($"Last number: {current}");
+
+
+
+
